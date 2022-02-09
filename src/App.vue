d22d39c4ca311bccff035dff691cb69764640816
ove4kina <v.ove4kina@gmail.com>
2022-02-09 12:21:31 +0300

    base template replacement

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Список валют</router-link> |
+    <router-link to="/converter">Конвертор</router-link>
   </div>
   <router-view/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Currencies from '../views/Currencies.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Currencies',
+    component: Currencies
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/converter',
+    name: 'Converter',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "Converter" */ '../views/Converter.vue')
   }
 ]
 
